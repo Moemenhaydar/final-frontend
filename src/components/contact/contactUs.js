c2fe99bcb1cd684740a5313eece7058ea8b9853d
Moemenhaydar <moemen.haydar98@gmail.com>
2023-06-01 09:51:46 +0300

    fixing the contact us

--- src/components/contact/contactUs.js
+++ src/components/contact/contactUs.js
@@ -8,23 +8,53 @@ const contactUs = () => {
         <h1 className="contact-us-heading">Contact Us</h1>
         <form className="contact-us-form">
           <div class="form-control">
-            <input type="value" required="" />
+            <input className="form-input" type="value" required="" />
             <label className="form-label">
-              <span className="form-span" style={{transitionDelay:"0ms"}}>U</span>
-              <span className="form-span" style={{transitionDelay:"50ms"}}>s</span>
-              <span className="form-span" style={{transitionDelay:"100ms"}}>e</span>
-              <span className="form-span" style={{transitionDelay:"150ms"}}>r</span>
-              <span className="form-span" style={{transitionDelay:"200ms"}}>n</span>
-              <span className="form-span" style={{transitionDelay:"250ms"}}>a</span>
-              <span className="form-span" style={{transitionDelay:"300ms"}}>m</span>
-              <span className="form-span" style={{transitionDelay:"350ms"}}>e</span>
+              <span className="form-span" style={{ transitionDelay: "0ms" }}>
+                U
+              </span>
+              <span className="form-span" style={{ transitionDelay: "50ms" }}>
+                s
+              </span>
+              <span className="form-span" style={{ transitionDelay: "100ms" }}>
+                e
+              </span>
+              <span className="form-span" style={{ transitionDelay: "150ms" }}>
+                r
+              </span>
+              <span className="form-span" style={{ transitionDelay: "200ms" }}>
+                n
+              </span>
+              <span className="form-span" style={{ transitionDelay: "250ms" }}>
+                a
+              </span>
+              <span className="form-span" style={{ transitionDelay: "300ms" }}>
+                m
+              </span>
+              <span className="form-span" style={{ transitionDelay: "350ms" }}>
+                e
+              </span>
             </label>
           </div>
-          <div className="form-field">
-            <label htmlFor="email" className="form-label">
-              Email
+          <div class="form-control">
+            <input className="form-input" type="value" required="" />
+            <label className="form-label">
+              <span className="form-span" style={{ transitionDelay: "0ms" }}>
+                E
+              </span>
+              <span className="form-span" style={{ transitionDelay: "50ms" }}>
+                M
+              </span>
+              <span className="form-span" style={{ transitionDelay: "100ms" }}>
+                A
+              </span>
+              <span className="form-span" style={{ transitionDelay: "150ms" }}>
+                I
+              </span>
+              <span className="form-span" style={{ transitionDelay: "200ms" }}>
+                L
+              </span>
             </label>
-            <input type="email" id="email" className="form-input" />
           </div>
           <div className="form-field">
             <label htmlFor="message" className="form-label">
@@ -36,9 +66,11 @@ const contactUs = () => {
               className="form-textarea"
             ></textarea>
           </div>
-          <button type="submit" className="form-button">
-            Submit
-          </button>
+          <div className="btn-contain">
+            <button type="submit" className="form-button">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
